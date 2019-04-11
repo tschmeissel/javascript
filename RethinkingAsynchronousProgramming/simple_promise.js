c5e1dc131d268b8c3987b9006730ce2b59892ba6
DESKTOP-74U9B4R\zaphod <torsten.schmeissel@gmx.de>
2019-04-11 15:47:11 +0200

    comment added

--- RethinkingAsynchronousProgramming/simple_promise.js
+++ RethinkingAsynchronousProgramming/simple_promise.js
@@ -10,6 +10,9 @@ function error() {
 
 function trackCheckout(info) {
 	return new Promise(function(resolve, reject) {
+		// here comes the processing code
+		// which leads to successful completion
+		// or an error.
 		resolve();
 	});
 }
