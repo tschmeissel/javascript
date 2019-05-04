5e1ca5b5c07bd814adb4d0c8aba2a28bd21f1280
DESKTOP-74U9B4R\zaphod <torsten.schmeissel@gmx.de>
2019-05-04 22:00:44 +0200

    more operators added

--- RxJs/completion_operators.js
+++ RxJs/completion_operators.js
@@ -20,3 +20,57 @@ const numbers = Rx.Observable.of(10, 100, 1000);
 numbers
 	.map(num => Math.log(num))
 	.subscribe(x => print(x));
+	
+const jsonString = '{"type": "Dog", "breed": "Pug"}'
+const apiCall = Rx.Observable.of(jsonString);
+
+apiCall
+	.map(json => JSON.parse(json))
+	.subscribe(obj => {
+		print(obj.type)
+		print(obj.breed)
+	})
+	
+// do
+
+const names = Rx.Observable.of("Simon", "Garfunkel");
+names
+	.do(name => print(name))
+	.map(name => name.toUpperCase())
+	.do(name => print(name))
+	.subscribe();
+
+// filter, first, last
+
+const unfiltered = Rx.Observable.of(-3, 5, 7, 2, -7, 9, -2);
+unfiltered
+	.filter(n => n>=0)
+	.subscribe(n => print(n));
+	
+unfiltered
+	.first()
+	.subscribe(n => print(n));
+	
+unfiltered
+	.last()
+	.subscribe(n => print(n));
+	
+// throttle, debounct
+
+// scan its like the reduce function
+
+// switchMap is useful to get an observable first before you can get another observable
+//  like when you need a user id before you can start something
+//  like you want to start a timer after some kind of event and stop it after another event
+
+// takeUntil can complete an observable based of a value from another observable
+
+// takeWhile will emit values from an observable until a certain condition becomes true
+
+// zip can combine observables
+
+// forkJoin is useful if you have a bunch of api call and you want to wait for all of them to resolve
+
+// catch to get errors happening while an observable is operating
+
+// retry to rerun an observable in case of an error
