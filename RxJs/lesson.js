5fe61583c164052eb645220d4a0f11b41b892605
DESKTOP-74U9B4R\zaphod <torsten.schmeissel@gmx.de>
2019-05-04 13:00:23 +0200

    removing not working reference

--- RxJs/lesson.js
+++ RxJs/lesson.js
@@ -1,5 +1,4 @@
 // https://www.youtube.com/watch?v=2LCo926NFLI
-// <script src="https://cdnjs.cloudflare.com/ajax/libs/rxjs/6.5.1/rxjs.umd.min.js.map"></script>
 
 // helper to print values
 function print(val) {
