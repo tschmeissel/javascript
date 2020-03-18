d20bcc3a76fe19b2dd7a8b0fc6606815c235cafd
DESKTOP-74U9B4R\zaphod <torsten.schmeissel@gmx.de>
2020-03-18 19:03:54 +0100

    scratch.txt added

--- snippets.js
+++ snippets.js
@@ -1,3 +1,19 @@
+export class G964 {
+  public static inArray(a1: string[], a2: string[]): string[] {
+  const result: Set<string> = new Set();
+
+  a2.forEach(str => {
+    a1.forEach(substr => {
+      if (str.search(substr) > 0) {
+        result.add(substr);
+      }
+    })
+  })
+
+  return Array.from(result).sort();
+  }
+}
+
 export class Challenge {
   /*
    check if every opening brace has a closing brace in proper order
