35fb27d1c1aa9bdf7725bf30151e160960f83e8c
DESKTOP-74U9B4R\zaphod <torsten.schmeissel@gmx.de>
2020-03-21 08:34:04 +0100

    update nach codewars

--- snippets.js
+++ snippets.js
@@ -1,3 +1,29 @@
+export function solution(roman: string): number {
+  const table = {
+    M : 1000,
+    D: 500,
+    C: 100,
+    L: 50,
+    X: 10,
+    V: 5,
+    I: 1
+  }
+  
+  return roman.split('').reduceRight((prev, cur, i, arr) => {
+    return table[arr[i+1]] > table[cur] ? prev - table[cur] : prev + table[cur];
+  }, 0);
+}
+
+const dictionary: { [ch: string]: number } = {
+  I: 1,
+  V: 5,
+  X: 10,
+  L: 50,
+  C: 100,
+  D: 500,
+  M: 1000,
+}
+  
 export class G964 {
   public static inArray(a1: string[], a2: string[]): string[] {
   const result: Set<string> = new Set();
