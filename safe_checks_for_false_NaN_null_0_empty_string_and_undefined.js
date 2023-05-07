640f9b2c3523e49dc4a4798d330843595906a771
Torsten Schmeissel <torsten.schmeissel@gmx.de>
2023-05-07 10:46:50 +0200

    add {} to evaluate as true

--- safe_checks_for_false_NaN_null_0_empty_string_and_undefined.js
+++ safe_checks_for_false_NaN_null_0_empty_string_and_undefined.js
@@ -13,6 +13,7 @@ if (value) {
     //     true
     //     " "
     //     <>0
+    //     {}
 }
 
 // Furthermore, if you do not know whether a variable exists (that means, if it was declared) you should check with the typeof operator. For instance
